d9ef0c011866f25c142b714f730156d4158433da
Vanaprastha <vanaagung781@gmail.com>
2025-10-01 21:42:25 +0700

    pertama

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,12 +1,11 @@
+import MapWrapper from "./MapWrapper";
 
-import dynamic from "next/dynamic";
-const MapSDG = dynamic(() => import("@/src/app/components/MapSDG"), { ssr: false });
 
 export default function DashboardPage() {
   return (
     <main className="p-6">
       <h1 className="text-xl font-bold mb-4">Peta SDGs 1 dengan Cluster</h1>
-      <MapSDG />
+      <MapWrapper />
     </main>
   );
 }
