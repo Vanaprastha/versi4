b800eee3cbf355be38712c6499e66b9709d895dd
Vanaprastha <vanaagung781@gmail.com>
2025-10-01 22:10:37 +0700

    pertama

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,11 +1,9 @@
-import MapWrapper from "./MapWrapper";
 
 
 export default function DashboardPage() {
   return (
     <main className="p-6">
       <h1 className="text-xl font-bold mb-4">Peta SDGs 1 dengan Cluster</h1>
-      <MapWrapper />
     </main>
   );
 }
