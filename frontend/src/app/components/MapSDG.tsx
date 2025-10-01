5fa69e33b04ca085aab77a864543d4516caae1fb
Vanaprastha <vanaagung781@gmail.com>
2025-10-01 21:58:37 +0700

    pertama

--- frontend/src/app/components/MapSDG.tsx
+++ frontend/src/app/components/MapSDG.tsx
@@ -60,6 +60,7 @@ export default function MapSDG() {
   return (
       // @ts-ignore
     <MapContainer center={center as any} zoom={13} style={{ height: "600px", width: "100%" }}>
+      // @ts-ignore
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
