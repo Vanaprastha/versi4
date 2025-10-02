a4d82a18e9638451d4789fac034ffbb2c28d5fe6
Vanaprastha <vanaagung781@gmail.com>
2025-10-02 21:39:00 +0700

    kedua

--- frontend/src/app/components/MapSDG.tsx
+++ frontend/src/app/components/MapSDG.tsx
@@ -84,6 +84,7 @@ export default function MapSDG({ goal }: Props) {
       {error && (
         <div className="mb-2 text-sm text-red-400">Gagal memuat data: {error}</div>
       )}
+      // @ts-ignore
       <MapContainer center={center as any} zoom={13} style={{ height: 420, width: "100%", borderRadius: 12 }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {data.map((v, idx) => {
